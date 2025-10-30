07e429d6a052039ed09355287bdf7d3a86c2fecb
av6985-glitch <av6985@g.rit.edu>
2025-10-30 16:20:35 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,17 @@
-// Represents a system user (customer or admin)
+// Represents a system user (customer)
+
+class User {
+    constructor(userId, name, email, password) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    register() {}
+    login() {}
+    viewBookings() {}
+    makeBooking() {}
+}
+
+module.exports = User;

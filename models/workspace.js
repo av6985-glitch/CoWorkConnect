f2d2aeed123a07591c36ff93a87fbc76fcdd1a7b
av6985-glitch <av6985@g.rit.edu>
2025-10-30 16:20:48 +0100

    Update workspace.js

--- models/workspace.js
+++ models/workspace.js
@@ -1,1 +1,17 @@
-// Represents an individual workspace or room that can be booked
+// Represents a workspace that can be booked
+
+class Workspace {
+    constructor(workspaceId, name, type, capacity, pricePerHour, availabilityStatus) {
+        this.workspaceId = workspaceId;
+        this.name = name;
+        this.type = type;
+        this.capacity = capacity;
+        this.pricePerHour = pricePerHour;
+        this.availabilityStatus = availabilityStatus;
+    }
+
+    updateAvailability() {}
+    getDetails() {}
+}
+
+module.exports = Workspace;

4b41bc33394c115abf9784d4189cdf9f0a55ab33
av6985-glitch <av6985@g.rit.edu>
2025-10-30 16:19:31 +0100

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,1 +1,17 @@
 // Represents an administrator with management privileges
+
+const User = require('./user');
+
+class Admin extends User {
+    constructor(adminId, name, email, password, role) {
+        super(adminId, name, email, password);
+        this.role = role;
+    }
+
+    addWorkspace() {}
+    updateWorkspace() {}
+    viewAllBookings() {}
+    manageUsers() {}
+}
+
+module.exports = Admin;

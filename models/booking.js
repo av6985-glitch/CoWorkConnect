481cf67b506bc7e2bca04b69773dfc5efb75a16f
av6985-glitch <av6985@g.rit.edu>
2025-10-30 16:19:45 +0100

    Update booking.js

--- models/booking.js
+++ models/booking.js
@@ -1,1 +1,19 @@
-// Represents a booking made by a user for a specific workspace
+// Represents a booking made by a user for a workspace
+
+class Booking {
+    constructor(bookingId, userId, workspaceId, date, startTime, endTime, status) {
+        this.bookingId = bookingId;
+        this.userId = userId;
+        this.workspaceId = workspaceId;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+    }
+
+    confirmBooking() {}
+    cancelBooking() {}
+    getBookingDetails() {}
+}
+
+module.exports = Booking;

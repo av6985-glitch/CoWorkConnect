89afea56f8f70a3e64769dce808c4f2fc4e453ea
av6985-glitch <av6985@g.rit.edu>
2025-10-30 16:20:22 +0100

    Update payment.js

--- models/payment.js
+++ models/payment.js
@@ -1,1 +1,15 @@
 // Handles payment-related data and transaction status
+
+class Payment {
+    constructor(paymentId, bookingId, amount, paymentStatus) {
+        this.paymentId = paymentId;
+        this.bookingId = bookingId;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+
+    processPayment() {}
+    generateReceipt() {}
+}
+
+module.exports = Payment;

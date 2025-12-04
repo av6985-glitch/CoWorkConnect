0a6938d97d8f87995fb28647d5f829b371746e85
dt8069-blip <dt8069@g.rit.edu>
2025-12-04 23:08:39 +0100

    Update adminController.js

--- js/controllers/adminController.js
+++ js/controllers/adminController.js
@@ -1,1 +1,22 @@
+import Workspace from "../models/workspace.js"
 
+export default class AdminController {
+
+    addWorkspace(name, price) {
+        const workspaces = JSON.parse(localStorage.getItem("workspaces") || "[]")
+
+        const w = new Workspace(Date.now(), name, price)
+        workspaces.push(w)
+
+        localStorage.setItem("workspaces", JSON.stringify(workspaces))
+        alert("Workspace added")
+    }
+
+    getAllWorkspaces() {
+        return JSON.parse(localStorage.getItem("workspaces") || "[]")
+    }
+
+    getAllBookings() {
+        return JSON.parse(localStorage.getItem("bookings") || "[]")
+    }
+}

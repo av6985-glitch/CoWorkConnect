7e9fa57ec935649ff912539d352d5706e09c9602
dt8069-blip <dt8069@g.rit.edu>
2025-12-04 23:09:45 +0100

    Update workspace.js

--- js/models/workspace.js
+++ js/models/workspace.js
@@ -1,1 +1,8 @@
-
+// js/models/workspace.js
+export default class Workspace {
+    constructor(id, name, price) {
+        this.id = id
+        this.name = name
+        this.price = price
+    }
+}

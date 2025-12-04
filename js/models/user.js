93178f130a8253c874fac7d97533ae52c1b9054f
Anid <av6985@g.rit.edu>
2025-12-04 23:14:31 +0100

    Update user.js

--- js/models/user.js
+++ js/models/user.js
@@ -1,1 +1,10 @@
-
+// js/models/user.js
+export default class User {
+    constructor(id, name, email, password, isAdmin = false) {
+        this.id = id
+        this.name = name
+        this.email = email
+        this.password = password
+        this.isAdmin = isAdmin
+    }
+}
